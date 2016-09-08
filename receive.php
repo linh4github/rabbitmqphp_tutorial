62c2dc27edf97dbab9888cd91b472d14219cb41e
linh.nguyen <ndl4business@gmail.com>
2016-09-08 23:31:58 +0700

    First Commit

---
+++ receive.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'admin', 'admin123');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+
+$callback = function($msg) {
+    echo " [x] Received ", $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while(count($channel->callbacks)) {
+    $channel->wait();
+}
